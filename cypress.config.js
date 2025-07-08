82035bd488d60fcc891227d3c239be2271ee448e
Alexsandro Ferreira <ferreira.alex@outlook.com>
2025-07-08 01:54:36 -0300

    feat: add report generation feature

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,21 @@
-/* eslint-disable no-unused-vars */ 
- 
-const { defineConfig } = require("cypress");
+/* eslint-disable no-unused-vars */
+
+const {
+  defineConfig
+} = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    baseUrl:  process.env.BASE_API_URL || 'https://dummyjson.com',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: false,
+    json: true
   },
+  e2e: {
+    baseUrl: process.env.BASE_API_URL || 'https://dummyjson.com',
+    setupNodeEvents(on, config) {
+      // plugins ou eventos aqui se necessário
+    }
+  }
 });
